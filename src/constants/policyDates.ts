704a182f160cc36874ed379e5a4897fe6ae2e650
SM1LE21 <lasknut@gmail.com>
2026-08-15 14:11:50 +0200

    chore(policy-dates): re-date the Privacy Policy for the Stripe sub-processor

--- src/constants/policyDates.ts
+++ src/constants/policyDates.ts
@@ -1,5 +1,5 @@
 export const POLICY_DATES = {
-  PRIVACY_POLICY: 'August 4, 2026',
+  PRIVACY_POLICY: 'August 15, 2026',
   COOKIE_POLICY: 'August 4, 2026',
   TERMS_OF_SERVICE: 'August 15, 2026'
 } as const;
